0a3303146622def52ab6897c831c7c699d91a489
nkemsileslie <nkemsileslie@gmail.com>
2023-07-08 21:44:36 -0400

    adding to the guessing game class

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+            int numberToGuess = random.nextInt(100) + 1;
+            int attempts = 0;
+            boolean guessed = false;
+
+            System.out.println("Welcome to the Guessing Game! ");
+            System.out.println("");
     }
 }
