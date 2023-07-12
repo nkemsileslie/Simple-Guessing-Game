af27d499c019a036147cb5a37bc4cb5b7c22fc07
nkemsileslie <nkemsileslie@gmail.com>
2023-07-12 10:26:38 -0400

    first commit on testing

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -6,18 +6,16 @@ public class GuessingGame {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-            int numberToGuess = random.nextInt(100) + 1;
+            int numberToGuess = random.nextInt(50) + 1;
             int attempts = 0;
             boolean guessed = false;
 
             System.out.println("Welcome to the Guessing Game! ");
-            System.out.println("I have selected a number between 1 and 100. Try to guess it!");
+            System.out.println("I have selected a number between 1 and 50. Try to guess it!");
 
             while (!guessed) {
                 System.out.print("Enter your guess: ");
-                int guess = scanner.nextInt();
-                attempts++;
-
+                int guess = scanner.nextInt();                  
                 if (guess == numberToGuess) {
                     System.out.println("Congratulations! you guesssed the right number");
                     System.out.println("Number of attempts: " + attempts);
